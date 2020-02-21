c1bf511da0bb4b23ffb96256b790c34122fed53e
JuhisVain <Juho.Vainio@student.laurea.fi>
2020-02-21 11:41:37 +0200

    Declare get-arg-count safe

--- plot.lisp
+++ plot.lisp
@@ -14,6 +14,7 @@
 (defun get-arg-count (func)
   "Returns count of number arguments that FUNC accepts,
 one of (1 2 NIL)"
+  (declare (optimize (safety 3)))
   (handler-case
       (progn
 	(funcall func 13)
